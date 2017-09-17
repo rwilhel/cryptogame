9930f4496c18800aa2ba659ee8e184563755d3d8
rwilhel <rwilhel@gmail.com>
2017-09-17 16:44:40 -0600

    Begins implementation of touch to jump functionality

--- cryptogame/GameScene.swift
+++ cryptogame/GameScene.swift
@@ -14,17 +14,25 @@ class GameScene: SKScene {
     var sky: EndlessBackground!
     var forest: EndlessBackground!
     var fence: EndlessBackground!
+    var cryptkeeper: SKSpriteNode!
     
     override func didMove(to view: SKView) {
         sky = EndlessBackground(parent: self, sprite: self.childNode(withName: "sky") as! SKSpriteNode, speed: 1)
         forest = EndlessBackground(parent: self, sprite: self.childNode(withName: "forest") as! SKSpriteNode, speed: 3)
         fence = EndlessBackground(parent: self, sprite: self.childNode(withName: "fence") as! SKSpriteNode, speed: 6)
+        
+        cryptkeeper = self.childNode(withName: "cryptkeeper") as! SKSpriteNode!
     }
     
     override func update(_ currentTime: TimeInterval) {
         sky.update()
         forest.update()
         fence.update()
-
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if cryptkeeper.physicsBody?.velocity.dy == 0 {
+            cryptkeeper.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 120))
+        }
     }
 }
